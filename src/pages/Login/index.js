bd20a9978ead0a8017c2d7e7445f3294517de0f3
usemrlin <lin950821@163.com>
2024-06-20 23:51:36 +0800

    完成登录静态页面和表单校验

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,51 @@
+import './index.scss'
+import { Card, Form, Input, Button} from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-    return <div>这是Login</div>
+    const onFinish = (value) => {
+        console.log(value);
+    }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            // 多条校验逻辑 先校验第一条 第一条通过之后再校验第二条
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号',
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号格式'
+              }
+            ]}>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码',
+              },
+            ]}>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
+
 export default Login
