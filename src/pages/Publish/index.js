0c42781839fbe8b260452697978f219c075a1778
usemrlin <lin950821@163.com>
2024-06-21 18:25:05 +0800

    获取频道列表数据展示

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -13,11 +13,25 @@ import {
 import { PlusOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "./index.scss";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useEffect, useState } from "react";
+import { getChannelAPI } from "@/apis/article";
 const { Option } = Select;
 
 const Publish = () => {
+  // 获取频道列表
+  const [channelList, setChannelList] = useState([]);
+
+  useEffect(() => {
+    // 封装函数 在函数内调用接口
+    const getChannelList = async () => {
+      const res = await getChannelAPI();
+      setChannelList(res.data.channels)
+    };
+    // 调用函数
+    getChannelList()
+  },[]);
   return (
     <div className="publish">
       <Card
@@ -48,21 +62,22 @@ const Publish = () => {
             rules={[{ required: true, message: "请选择文章频道" }]}
           >
             <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-              <Option value={0}>推荐</Option>
+              {/* value是选中之后的值 */}
+              {channelList.map(item=><Option key={item.id} value={item.id}>{item.name}</Option>)}
             </Select>
           </Form.Item>
-        
+
           <Form.Item
             label="内容"
             name="content"
             rules={[{ required: true, message: "请输入文章内容" }]}
           >
-          {/* 富文本编辑器 */}
-          <ReactQuill 
-          className="publish-quill"
-          theme="snow"
-          placeholder="请输入文章内容"
-          />
+            {/* 富文本编辑器 */}
+            <ReactQuill
+              className="publish-quill"
+              theme="snow"
+              placeholder="请输入文章内容"
+            />
           </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
