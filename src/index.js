435418e99d8f88e68ebef35e9faa0dcf6a4b71a6
usemrlin <lin950821@163.com>
2024-06-21 18:02:41 +0800

    加入富文本编辑器

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import store from "./store";
 import 'normalize.css'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>
 );
--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -13,7 +13,8 @@ import {
 import { PlusOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "./index.scss";
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 const { Option } = Select;
 
 const Publish = () => {
@@ -56,6 +57,12 @@ const Publish = () => {
             name="content"
             rules={[{ required: true, message: "请输入文章内容" }]}
           >
+          {/* 富文本编辑器 */}
+          <ReactQuill 
+          className="publish-quill"
+          theme="snow"
+          placeholder="请输入文章内容"
+          />
           </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
