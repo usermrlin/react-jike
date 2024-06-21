92d7487a3e4df846500c6d98ac7960816ca2b967
usemrlin <lin950821@163.com>
2024-06-21 14:12:59 +0800

    首页静态页面和样式初始化

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,61 @@
-import { request } from "@/utils/request";
-import { useEffect } from "react";
-const Layout = () => {
-    useEffect(()=> {
-        request.get('/user/profile')
-    })
-  return <div>这是Layout</div>;
-};
-export default Layout;
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons'
+import './index.scss'
+
+const { Header, Sider } = Layout
+
+const items = [
+  {
+    label: '首页',
+    key: '/',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: '文章管理',
+    key: '/article',
+    icon: <DiffOutlined />,
+  },
+  {
+    label: '创建文章',
+    key: '/publish',
+    icon: <EditOutlined />,
+  },
+]
+
+const GeekLayout = () => {
+
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <div className="user-info">
+          <span className="user-name"></span>
+          <span className="user-logout">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            theme="dark"
+            items={items}
+            style={{ height: '100%', borderRight: 0 }}></Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          {/* 二级路由的出口 */}
+          {/* <Outlet /> */}
+        </Layout>
+      </Layout>
+    </Layout>
+  )
+}
+export default GeekLayout
