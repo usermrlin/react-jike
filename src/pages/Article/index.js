61ec483f93d371259dda94b8388f76f6da613e9c
usemrlin <lin950821@163.com>
2024-06-22 16:35:11 +0800

    文章列表静态结构

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,5 +1,158 @@
-const Article = () => {
-    return <div>this is Article</div>
-}
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  DatePicker,
+  Select,
+  Popconfirm,
+} from "antd";
+// 引入汉化包 时间选择器显示中文
+import locale from "antd/es/date-picker/locale/zh_CN";
 
-export default Article
+import { Table, Tag, Space } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import img404 from "@/assets/error.png";
+
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+
+const Article = () => {
+  const columns = [
+    {
+      title: "封面",
+      dataIndex: "cover",
+      width: 120,
+      render: (cover) => {
+        return (
+          <img src={cover.images[0] || img404} width={80} height={60} alt="" />
+        );
+      },
+    },
+    {
+      title: "标题",
+      dataIndex: "title",
+      width: 220,
+    },
+    {
+      title: "状态",
+      dataIndex: "status",
+      // data - 后端返回的状态status 根据它做条件渲染
+      // data === 1 => 待审核
+      // data === 2 => 审核通过
+      render: (data) => [data],
+    },
+    {
+      title: "发布时间",
+      dataIndex: "pubdate",
+    },
+    {
+      title: "阅读数",
+      dataIndex: "read_count",
+    },
+    {
+      title: "评论数",
+      dataIndex: "comment_count",
+    },
+    {
+      title: "点赞数",
+      dataIndex: "like_count",
+    },
+    {
+      title: "操作",
+      render: (data) => {
+        return (
+          <Space size="middle">
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<EditOutlined />}
+            />
+            <Popconfirm
+              title="删除文章"
+              description="确认要删除当前文章吗?"
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button
+                type="primary"
+                danger
+                shape="circle"
+                icon={<DeleteOutlined />}
+              />
+            </Popconfirm>
+          </Space>
+        );
+      },
+    },
+  ];
+  // 准备表格body数据
+  const data = [
+    {
+      id: "8218",
+      comment_count: 0,
+      cover: {
+        images: [],
+      },
+      like_count: 0,
+      pubdate: "2019-03-11 09:00:00",
+      read_count: 2,
+      status: 2,
+      title: "wkwebview离线化加载h5资源解决方案",
+    },
+  ];
+  return (
+    <div>
+      <Card
+        title={
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>首页</Link> },
+              { title: "文章列表" },
+            ]}
+          />
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form initialValues={{ status: "" }}>
+          <Form.Item label="状态" name="status">
+            <Radio.Group>
+              <Radio value={""}>全部</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="频道" name="channel_id">
+            <Select placeholder="请选择文章频道" style={{ width: 120 }}>
+              <Option></Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="日期" name="date">
+            {/* 传入locale属性 控制中文显示*/}
+            <RangePicker locale={locale}></RangePicker>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ marginLeft: 40 }}>
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+      {/* 表格区域 */}
+      <Card title={`根据筛选条件共查询到 ${1} 条结果：`}>
+        <Table
+          rowKey="id"
+          columns={columns}
+          dataSource={data}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default Article;
